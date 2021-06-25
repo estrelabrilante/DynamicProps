e9524230324063e8dbad643ac0e1489f3d69dbfd
Shynu Mary Varghese <Svarghese2938@conestogac.on.ca>
2021-06-26 01:39:56 +0530

    working with dynamic props

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,57 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite ? "[Added to favorite]" : " " }}</h2>
+    <button @click="toggleFavorite">Add favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.friendIsFavorite = !this.friendIsFavorite;
+    },
+  },
+};
+</script>
